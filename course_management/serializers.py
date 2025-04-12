d6ee1f896e67c75ceaf8f693782ac37e86e104b3
Osigelialex <osigelialex@gmail.com>
2025-04-13 00:25:17 +0100

    fix: fixed course validation regex

--- course_management/serializers.py
+++ course_management/serializers.py
@@ -30,7 +30,7 @@ class CourseSerializer(serializers.ModelSerializer):
         return value
     
     def validate_course_code(self, course_code):
-        pattern = '^CSC\d{3}$'
+        pattern = r'^CSC\d{3}$'
         if not re.match(pattern, course_code):
             raise serializers.ValidationError('Course code must follow the format CSCxxx')
         return course_code
